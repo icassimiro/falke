c0fcc62f64cce71fde194e0cdd67e75b7a51dd39
icassimiro <icassimiro@hotmail.com>
2025-01-10 09:59:54 -0300

    css

--- src/Slide.js
+++ src/Slide.js
@@ -23,7 +23,7 @@ export default function Slide() {
                         <img className="logo" src={logo}></img>
                         <h2 className="titlebeer">GERMAN PILSEN</h2>
                         <h2 className="subtitle">PILSEN</h2>
-                        <h3 className="textbeer">Cerveja puro <strong className="strong">malte</strong>, com lúpulos nobres alemães suave. Apresenta uma <strong className="strong">coloração</strong> amarelo-palha, <strong className="strong">creme</strong> bem formado e com média persistência.</h3>
+                        <h3 className="textbeer">Cerveja puro <strong className="strong">malte</strong>, com lúpulos nobres alemães suave. Apresenta uma <strong className="strong">coloração</strong> amarelo-palha, creme bem formado e com média <strong className="strong">persistência</strong>.</h3>
                         <h4 className="abv">4,5% ABV | 20 IBU</h4>
                     </div>
                 </div>
@@ -43,7 +43,7 @@ export default function Slide() {
                         <img className="logo" src={logo}></img>
                         <h2 className="titlebeer">HOP LAGER</h2>
                         <h2 className="subtitle">LAGER</h2>
-                        <h3 className="textbeer">Leve e refrescante, amargor moderado e baixo teor alcoólico, mas com intenso aroma de lúpulos americanos, trazendo notas cítricas e frutadas.</h3>
+                        <h3 className="textbeer">Leve e refrescante, <strong className="strong">amargor</strong> moderado e baixo teor alcoólico, mas com intenso aroma de <strong className="strong">lúpulos</strong> americanos, trazendo notas <strong className="strong">cítricas</strong> e frutadas.</h3>
                         <h4 className="abv">4,5% ABV | 20 IBU</h4>
                     </div>
                 </div>
@@ -53,7 +53,7 @@ export default function Slide() {
                         <img className="logo" src={logo}></img>
                         <h2 className="titlebeer">DIAMANTINA</h2>
                         <h2 className="subtitle">BOHEMIAN PILSNER</h2>
-                        <h3 className="textbeer">Coloração amarelo-ouro, corpo médio e espuma de boa formação. Aromas de cereal maltado e sabor equilibrado entre o malte e o lúpulo Saaz.</h3>
+                        <h3 className="textbeer">Coloração <strong className="strong">amarelo-ouro</strong>, corpo médio e espuma de boa formação. Aromas de cereal <strong className="strong">maltado</strong> e sabor equilibrado entre o malte e o lúpulo <strong className="strong">Saaz</strong>.</h3>
                         <h4 className="abv">5,7% ABV | 50 IBU</h4>
                     </div>
                 </div>
@@ -63,8 +63,8 @@ export default function Slide() {
                         <img className="logo" src={logo}></img>
                         <h2 className="titlebeer">OURO PRETO</h2>
                         <h2 className="subtitle">SCHWARZBIER</h2>
-                        <h3 className="textbeer">Coloração negro avermelhada, espuma de boa formação e persistente. Aromas de malte torrado e café, corpo médio e sensação de café e cacau.</h3>
-                        <h4 className="abv">5,4% ABV | 20 IBU</h4>
+                        <h3 className="textbeer">Coloração <strong className="strong">negro avermelhada</strong>, espuma de boa formação e persistente. Aromas de malte torrado e <strong className="strong">café</strong>, corpo médio e sensação de café e <strong className="strong">cacau</strong>.</h3>
+                        <h4 className="abv">5,4% ABV | 20 IBU</h4>  
                     </div>
                 </div>
                 <div className="divcarousel">
@@ -73,7 +73,7 @@ export default function Slide() {
                     <img className="logo" src={logo}></img>
                         <h2 className="titlebeer">PEREGRINUS</h2>
                         <h2 className="subtitle">AMERICAN PALE ALE</h2>
-                        <h3 className="textbeer">Cerveja de cor amarelo claro, espuma branca e cremosa. Aroma intenso dos lúpulos americanos, cítricos e frutados. Corpo leve e refrescante.</h3>
+                        <h3 className="textbeer">Cerveja de cor <strong className="strong">amarelo claro</strong>, espuma branca e cremosa. Aroma intenso dos lúpulos americanos, cítricos e <strong className="strong">frutados</strong>. Corpo leve e <strong className="strong">refrescante</strong>.</h3>
                         <h4 className="abv">5,2% ABV | 34 IBU</h4>
                     </div>
                 </div>
